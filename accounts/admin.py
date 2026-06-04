190dc4080ab6f27b97c0776846ad95e0dcaafc12
Dakshita-Bhardwaj <bhardwajd2039@gmail.com>
2026-06-04 23:50:22 +0530

    Refactor posts API to CBVs and improve admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'is_staff',
+        'date_joined'
+    )
+
+    search_fields = (
+        'username',
+        'email'
+    )
+
+    list_filter = (
+        'is_staff',
+        'is_superuser',
+        'is_active'
+    )
